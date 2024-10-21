71623b6958b93121f83cfe2a4ebd53b5d66a8d18
rsarkarr <97837402+rsarkarr@users.noreply.github.com>
2024-10-21 15:04:00 +0530

    check cookie

--- src/pages/Profile.tsx
+++ src/pages/Profile.tsx
@@ -1,4 +1,39 @@
+import Cookies from "js-cookie"
+import { useEffect } from "react"
+import { useNavigate } from "react-router-dom";
+
+
 const Profile = () => {
+
+  const authToken = Cookies.get("sessionToken")
+  console.log(authToken);
+  const navigate = useNavigate()
+  
+
+  useEffect(() => {
+    const checkValidUser = async () => {
+      try {
+        const sendReq = await fetch("http://localhost:3333/api/v1/checkvalidjwt", {
+          method: "POST",
+          headers: {
+            Authorization: `${authToken}`
+          },
+          credentials: "include"
+        })
+
+        const apiRes = await sendReq.json()
+        if (apiRes.success === false) {
+          navigate("/login")
+        }
+        
+      } catch (error) {
+        console.log(error);
+      }
+    }
+
+    checkValidUser()
+  }, [navigate])
+
   return (
     <div>Profile</div>
   )
